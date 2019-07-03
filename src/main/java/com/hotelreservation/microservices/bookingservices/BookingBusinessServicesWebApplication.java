88717c0bd0d1402e7abf334f227878f19b5cbad1
e068635 <elson.franca@mastercard.com>
2019-07-03 12:01:27 -0300

    removed unused imports

--- src/main/java/com/hotelreservation/microservices/bookingservices/BookingBusinessServicesWebApplication.java
+++ src/main/java/com/hotelreservation/microservices/bookingservices/BookingBusinessServicesWebApplication.java
@@ -3,8 +3,7 @@ package com.hotelreservation.microservices.bookingservices;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+
 
 
 @SpringBootApplication
